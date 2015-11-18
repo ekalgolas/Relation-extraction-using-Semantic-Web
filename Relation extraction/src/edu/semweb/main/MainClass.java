1048e202cc9ebe95247e90d6288696f0d53167e8
ekalgolas <exg140230@utdallas.edu>
2015-11-18 00:33:56 -0600

    Fixing reading model from ontology

--- Relation extraction/src/edu/semweb/main/MainClass.java
+++ Relation extraction/src/edu/semweb/main/MainClass.java
@@ -9,11 +9,12 @@ import edu.semweb.nlp.RelationsInDocument;
 public class MainClass {
 	public static void main(final String[] args)
 			throws IOException {
+		final CustomOntology customOntology = new CustomOntology();
+
 		final EntityRelationExtractor ere = new EntityRelationExtractor();
 		final RelationsInDocument rd = ere.getRelationships("http://www.doc.url",
 				"Satya Nadella is the CEO of Microsoft. It's headquarters is in Redmond, Washington. He was born in India. She lives in Dallas, Texas. She sold the company for $1000.");
 
-		final CustomOntology customOntology = new CustomOntology();
 		customOntology.getXMLFromOntology(rd);
 	}
 }
--- Relation extraction/src/ontology/CustomOntology.java
+++ Relation extraction/src/ontology/CustomOntology.java
@@ -1,17 +1,17 @@
 package ontology;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.jena.query.Dataset;
-import org.apache.jena.rdf.model.Model;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.tdb.TDBFactory;
-import org.apache.jena.util.FileManager;
 
 import edu.semweb.nlp.RelationsInDocument;
 import edu.semweb.nlp.RelationsInSentence;
@@ -24,20 +24,15 @@ import edu.semweb.nlp.RelationsInSentence;
 public class CustomOntology {
 	public File getXMLFromOntology(final RelationsInDocument rd)
 			throws IOException {
-		org.apache.log4j.Logger.getRootLogger()
-		.setLevel(org.apache.log4j.Level.OFF);
+		org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
 
-		// Make a TDB-backed dataset
-		final String directory = "data/MyDatabases/";
-		final Dataset dataset = TDBFactory.createDataset(directory + "Dataset1");
-
-		// create an empty Model
-		final Model model = dataset.getDefaultModel();
+		// Create an empty Model
+		final String file = "data/Ontology.owl";
+		final OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM_RDFS_INF);
 
 		// Load Monterey and record time
 		final double start = System.currentTimeMillis();
-		final String file = "data/Ontology.owl";
-		FileManager.get().readModel(model, file);
+		model.read(new FileInputStream(new File(file)), "RDF/XML");
 		final double end = System.currentTimeMillis();
 
 		// Display the time taken
@@ -52,9 +47,8 @@ public class CustomOntology {
 
 			for (final String entry : map.keySet()) {
 				final Resource resource = model.createResource("http://www.semanticweb.org/Relation-extraction#" + entry);
-				resource.addProperty(property, map.get(entry)
-					.get(0));
-				System.out.println(sentence + " : " + map.get(entry));
+				resource.addProperty(property, map.get(entry).get(0));
+				System.out.println(sentence + " : " + map.get(entry) + " : " + entry);
 			}
 		}
 
